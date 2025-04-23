f55aff9a265b499470a2eaa8ac40ce9dc9dd78cd
MarkProMaster229 <164912605+MarkProMaster229@users.noreply.github.com>
2025-04-23 14:24:20 +0300

    Add files via upload

---
+++ test/test_routes.py
@@ -0,0 +1,22 @@
+from inside import app  # Путь к твоему приложению
+from unittest.mock import patch
+import pytest
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+@patch('inside.db')  # Здесь также указываем правильный путь
+def test_login_success(mock_db, client):
+    mock_db.check_user.return_value = True
+
+    response = client.post('/', data={
+        'username': 'testuser',
+        'password': 'secret',
+        'action': 'login'
+    }, follow_redirects=False)
+
+    assert response.status_code == 302
+    assert '/rol' in response.headers['Location']
